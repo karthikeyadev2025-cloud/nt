8c8411798668345aff08b76ce3eb9ed05adb57f8
Karthi <karthikeyadev2025@gmail.com>
2026-07-30 10:25:09 +0530

    Update KiteTailLogo: Render pure vector kite icon matching user provided brand mark with text removed

--- src/components/KiteTailLogo.tsx
+++ src/components/KiteTailLogo.tsx
@@ -1,27 +1,18 @@
-export function KiteTailLogo({ className = "w-8 h-8", showText = false }: { className?: string; showText?: boolean }) {
+export function KiteTailLogo({ className = "w-8 h-8" }: { className?: string }) {
   return (
-    <div className="inline-flex items-center gap-2.5 select-none">
-      <svg className={className} viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-        {/* Geometric Kite Body - Deep Blue Left Facet */}
-        <path d="M50 8 L18 42 L50 68 Z" fill="#1D4ED8" />
-        {/* Geometric Kite Body - Bright Sapphire Right Facet */}
-        <path d="M50 8 L82 32 L50 68 Z" fill="#2563EB" />
-        
-        {/* Crisp White 'N' Logo inside Kite */}
-        <path d="M35 50 L35 22 L42 22 L56 44 L56 22 L63 22 L63 50 L56 50 L42 28 L42 50 Z" fill="#FFFFFF" />
-        
-        {/* Swooping Tail Line with Ascending Arrow */}
-        <path d="M48 68 C35 78 20 84 10 88 C25 84 40 78 52 71 C45 78 30 84 10 88 C32 82 46 74 48 68 Z" fill="#0284C7" />
-        <path d="M46 70 Q60 80 82 72 L72 88 L70 78 Q55 86 46 70 Z" fill="#0369A1" />
-        {/* Growth Arrow Tail tip */}
-        <path d="M52 68 C65 74 76 70 88 56 L80 58 L86 44 L72 50 L76 56 C66 65 57 67 52 68 Z" fill="#38BDF8" />
-      </svg>
-      {showText && (
-        <div className="flex flex-col text-left leading-none">
-          <span className="text-slate-900 font-extrabold text-sm tracking-wider uppercase">KITE &amp; TAIL</span>
-          <span className="text-blue-700 font-bold text-[10px] tracking-tight">BY NIKKI TECHNOLOGIES</span>
-        </div>
-      )}
-    </div>
+    <svg className={className} viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+      {/* Tilted Diamond Kite Facet - Left Executive Blue */}
+      <path d="M54 6 L18 36 L54 60 Z" fill="#1D4ED8" />
+      {/* Tilted Diamond Kite Facet - Right Royal Blue */}
+      <path d="M54 6 L86 28 L54 60 Z" fill="#2563EB" />
+      
+      {/* White Geometric 'N' Mark inside Kite */}
+      <path d="M38 46 L38 20 L45 20 L58 40 L58 20 L65 20 L65 46 L58 46 L45 26 L45 46 Z" fill="#FFFFFF" />
+      
+      {/* Ascending Growth Arrow Tail swooping out of Kite */}
+      <path d="M52 60 C35 72 20 80 8 84 C28 78 44 70 52 60 Z" fill="#0284C7" />
+      <path d="M18 82 C38 76 60 70 82 48 L74 52 L88 34 L76 48 L82 48 C60 70 38 76 18 82 Z" fill="#0284C7" />
+      <path d="M78 46 L88 34 L72 42 L77 47 L78 46 Z" fill="#38BDF8" />
+    </svg>
   );
 }
